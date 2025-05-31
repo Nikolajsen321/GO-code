2d44583fc0f31eb3da16a92461f6722d28c81df6
Nikolajsen321 <143070701+Nikolajsen321@users.noreply.github.com>
2025-05-31 16:42:53 +0200

    API  endpoint without gin

--- golang/variabler/api_funktioner/variabler_api_func.go
+++ golang/variabler/api_funktioner/variabler_api_func.go
@@ -1,11 +1,29 @@
 package apifunktioner
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
-func RenteHandler(rs http.Response, rq http.Request) {
-	query := r.URL.Query()
-	startS
+func RenteHandler(rs http.ResponseWriter, rq *http.Request) {
+	query := rq.URL.Query()
+	startStr := query.Get("start")
+	renteStr := query.Get("rente")
+	aarStr := query.Get("aar")
+
+	start, _ := strconv.ParseFloat(startStr, 64)
+	rente, _ := strconv.ParseFloat(renteStr, 64)
+	aar, _ := strconv.Atoi(aarStr)
+
+	slut := start
+	for i := 0; i < aar; i++ {
+		slut *= (1 + rente)
+	}
+
+	rs.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rs).Encode(map[string]float64{
+		"slutkapital": slut,
+	})
 
 }
